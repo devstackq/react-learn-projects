aa5a4fdae974d18f9cb61d7769480bdf1505c57a
devstackq <loginpsn@mail.ru>
2020-01-14 12:34:40 +0600

    start movie cards

---
+++ movie-cards/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Movie from './Movie'
+
+//https://github.com/drminnaar/react-movie-cards/blob/master/src/components/App.js
+function App() {
+  return (
+    <div className="App">
+  < Movie />
+    </div>
+  );
+}
+
+export default App;
